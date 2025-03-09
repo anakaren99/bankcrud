1b93d71e2cfdbe98fb478da31a3bcf49fb661d85
anakaren <anakarenpineda2@gmail.com>
2025-03-09 16:21:17 -0600

    Agregar documentacion con Swagger

--- src/main/java/com/sivale/bankcrud/Controller/ConsultaCuentasController.java
+++ src/main/java/com/sivale/bankcrud/Controller/ConsultaCuentasController.java
@@ -2,6 +2,10 @@ package com.sivale.bankcrud.Controller;
 
 import com.sivale.bankcrud.Service.ConsultaCuentasService;
 import com.sivale.bankcrud.dto.ResponseConsultaCuentas;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +19,10 @@ public class ConsultaCuentasController {
     @Autowired
     private ConsultaCuentasService consultaCuentasService;
 
+    @Operation(summary = "Obtiene todas las cuentas", description = "Retorna una lista de cuentas disponibles")
+    @ApiResponse(responseCode = "200", description = "Lista de cuentas obtenida correctamente",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ResponseConsultaCuentas.Cuentas.class)))
     @GetMapping("/getInfoClient/{id}")
     public ResponseConsultaCuentas getInfoClient(@PathVariable Long id) {
        return consultaCuentasService.getCuentas(id);
